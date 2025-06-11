1a1029ccc48a6a9dd826d13f383ba560370027b2
Michaelchau1002 <chauchiiat@gmail.com>
2025-06-11 21:26:43 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+function main() {
+    (function () {
+        'use strict';
+        
+        /* Page Scrolling */
+        $('a.scroll-nav').click(function() {
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top - 40
+                    }, 900);
+                    return false;
+                }
+            }
+        });
+
+        /* Show Menu on Scroll */
+        $(window).bind('scroll', function() {
+            var navHeight = $(window).height() - 100;
+            if ($(window).scrollTop() > navHeight) {
+                $('.navbar-default').addClass('on');
+            } else {
+                $('.navbar-default').removeClass('on');
+            }
+        });
+        
+        /* Scroll Spy */
+        $('body').scrollspy({
+            target: '.navbar-default',
+            offset: 80
+        });
+        
+        // Simple form submission handler
+        $('#contactform').submit(function(e) {
+            e.preventDefault();
+            alert('Thank you for your message! I will get back to you soon.');
+            this.reset();
+        });
+        
+    })();
+}
+main();
