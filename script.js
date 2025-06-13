55fe283d4bb2f47f7c9d4ded07bbd58db462681f
Michaelchau1002 <chauchiiat@gmail.com>
2025-06-13 14:50:15 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 $(document).ready(function() {
-    // 页面加载动画
     window.addEventListener('load', function() {
         setTimeout(function() {
             document.getElementById('loader').classList.add('hidden');
         }, 800);
     });
    
-    // 导航栏滚动效果
     $(window).bind('scroll', function() {
         var navHeight = $(window).height() - 100;
         if ($(window).scrollTop() > navHeight) {
@@ -15,11 +13,9 @@ $(document).ready(function() {
             $('.navbar-default').removeClass('on');
         }
        
-        // 滚动触发动画
         checkScrollAnimation();
     });
    
-    // 平滑滚动
     $('a.scroll-nav').click(function() {
         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
             var target = $(this.hash);
@@ -33,14 +29,12 @@ $(document).ready(function() {
         }
     });
    
-    // 联系表单提交
     $('#contactform').submit(function(e) {
         e.preventDefault();
         alert('Thank you for your message! I will get back to you soon.');
         this.reset();
     });
    
-    // 图片模态框
     var modal = document.getElementById('imageModal');
     var imgElements = document.querySelectorAll('.gallery-image, .award-img-placeholder, .certificate-img-placeholder, .project-img-placeholder');
     var modalImg = document.getElementById("modalImage");
@@ -56,24 +50,20 @@ $(document).ready(function() {
         });
     });
    
-    // 关闭模态框
     document.getElementById("imageModal").querySelector('.close').addEventListener('click', function() {
         modal.style.display = "none";
     });
    
-    // 点击模态框外部关闭
     window.addEventListener('click', function(event) {
         if (event.target == modal) {
             modal.style.display = "none";
         }
     });
    
-    // 滚动动画检测
     function checkScrollAnimation() {
         var windowHeight = $(window).height();
         var scrollPosition = $(window).scrollTop();
        
-        // 为所有带有动画类的元素添加可见类
         $('.animated').each(function() {
             var elementTop = $(this).offset().top;
             if (elementTop < scrollPosition + windowHeight - 100) {
@@ -81,7 +71,6 @@ $(document).ready(function() {
             }
         });
        
-        // 触发技能进度条动画
         if ($('#about').offset().top < scrollPosition + windowHeight - 100 && !$('#about').hasClass('animated-started')) {
             $('#about').addClass('animated-started');
             setTimeout(function() {
@@ -96,6 +85,5 @@ $(document).ready(function() {
         }
     }
    
-    // 初始化动画检测
     checkScrollAnimation();
 });
